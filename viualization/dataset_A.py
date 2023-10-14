027ba26f3d721e3091881f44598d88ccb991afd2
alexOarga <alex718123@gmail.com>
2023-10-14 13:03:11 +0200

    Change dataset histograms

--- viualization/dataset_A.py
+++ viualization/dataset_A.py
@@ -4,13 +4,14 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 db_a = pd.read_csv('../database/train_A_derma.csv')
-
 db_a['Fake/Real'] = db_a['Fake/Real'].replace({'real': 0, 'fake': 1})
-
 print(db_a.info())
-
 sns.pairplot(db_a, hue='Fake/Real')
+plt.savefig('pairplot_dataset_a_train.jpg')
 
-plt.savefig('pairplot_dataset_a.jpg')
+db_a = pd.read_csv('../database/test_A_derma.csv')
+print(db_a.info())
+sns.pairplot(db_a)
+plt.savefig('pairplot_dataset_test.jpg')
 
 
