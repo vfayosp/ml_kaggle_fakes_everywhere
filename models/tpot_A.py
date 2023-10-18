aa2be60d5cb141dd634b195f70b3cc8f994edc9d
vfayos <fayos99@gmail.com>
2023-10-18 17:17:36 +0200

    Added the tpot model for dataset A

---
+++ models/tpot_A.py
@@ -0,0 +1,16 @@
+import pandas as pd
+from tpot import TPOTClassifier
+from sklearn.model_selection import train_test_split
+
+df=pd.read_csv('../database/train_A_derma.csv')
+Y = df['Fake/Real'].replace({'real': 0, 'fake': 1})
+
+X = df.drop(['Fake/Real', 'Doughnuts consumption', 'Id'], axis=1)
+
+X_train, X_test, y_train, y_test = train_test_split(X,Y, test_size=0.1, shuffle=True, random_state=42)
+
+pipeline_optimizer = TPOTClassifier(generations=5, population_size=20, cv=5,
+                                    random_state=42, verbosity=2, config_dict='TPOT light')
+
+pipeline_optimizer.fit(X_train, y_train)
+print(pipeline_optimizer.score(X_test, y_test))
